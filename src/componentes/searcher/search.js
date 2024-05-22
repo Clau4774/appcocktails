7adeba98de7860c9ef47d96797eade674ccdfab5
claudioDang47 <claudiodangiolo@gmail.com>
2024-05-22 10:39:08 -0300

    Working on search bar, adjusting CSS grid for movile formats

--- src/componentes/searcher/search.js
+++ src/componentes/searcher/search.js
@@ -1,16 +1,8 @@
-import { searchType } from "./searchType";
 import data from '../../data/cocktails.json'
 
 
-export const search = (busqueda, tipoBusqueda) => {
+export const search = (busqueda) => {
 
-    switch(tipoBusqueda) {
-
-      case searchType.titulo : return console.log(tipoBusqueda);
-      case searchType.ingrediente : return console.log(tipoBusqueda);
-      case searchType.alcohol : return console.log(tipoBusqueda);
-      default: console.log('wiii');
-    }
-
-    console.log(data.find(elem => (elem.titulo.toLowerCase()) === busqueda))
+  console.log(busqueda)
+    
   }
--- src/componentes/searcher/searchType.js
+++
@@ -1,5 +0,0 @@
-export const searchType = {
-    titulo: 'nombre',
-    ingrediente: 'ingrediente',
-    alcohol: 'alcohol'
-  }
